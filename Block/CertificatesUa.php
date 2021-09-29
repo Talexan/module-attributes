746ac6716e3052417bd46e1bd007f2a3906644df
Talexan <lKotov.mihaill@gmail.com>
2021-09-29 18:58:40 +0300

    No extra constructor

--- Block/CertificatesUa.php
+++ Block/CertificatesUa.php
@@ -5,36 +5,6 @@ namespace Talexan\Attributes\Block;
 class CertificatesUa extends \Magento\Catalog\Block\Product\View {
 
     /**
-     * @var \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
-     */
-    private $eavSetupFactory;
-
-    /**
-     * @inheritdoc
-     */
-    public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
-        \Magento\Framework\Url\EncoderInterface $urlEncoder,
-        \Magento\Framework\Json\EncoderInterface $jsonEncoder,
-        \Magento\Framework\Stdlib\StringUtils $string,
-        \Magento\Catalog\Helper\Product $productHelper,
-        \Magento\Catalog\Model\ProductTypes\ConfigInterface $productTypeConfig,
-        \Magento\Framework\Locale\FormatInterface $localeFormat,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
-        \Magento\Framework\Pricing\PriceCurrencyInterface $priceCurrency,
-        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory,
-        array $data = []
-    )
-    {
-        parent::__construct($context, $urlEncoder, $jsonEncoder, $string, $productHelper,
-                    $productTypeConfig, $localeFormat, $customerSession, $productRepository,
-                    $priceCurrency, $data);
-        
-        $this->eavSetupFactory = $eavSetupFactory;
-    }
-
-    /**
      * retrieve custom attribute certificates_ukr
      * @return string html
      */
